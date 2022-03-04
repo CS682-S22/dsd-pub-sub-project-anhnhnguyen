a4d4717ed20212580c17413841489f7ab4125aa6
anhnhnguyen <anguyen.09@icloud.com>
2022-03-04 15:33:44 -0800

    added test to ensure lots of message was sent

--- src/test/java/project2/producer/ProducerTest.java
+++ src/test/java/project2/producer/ProducerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProducerTest {
 
     @Test
-    void testSuccessConnection() {
+    void testSuccessSimpleConnection() {
         String topic = "test";
         byte[] data = "this is a test".getBytes(StandardCharsets.UTF_8);
         Thread t = new Thread(() -> {
@@ -41,6 +41,49 @@ class ProducerTest {
         t.start();
         Producer producer = new Producer("localhost", 1024);
         producer.send(topic, data);
+        producer.close();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void testSuccessComplexConnection() {
+        String topic = "test";
+        byte[] data = "this is a test".getBytes(StandardCharsets.UTF_8);
+        Thread t = new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(1024);
+                Socket socket = serverSocket.accept();
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                int length = dis.readInt();
+                while (length > 0) {
+                    byte[] message = new byte[length];
+                    dis.readFully(message, 0, length);
+                    Message.Wrapper wrapper = Message.Wrapper.parseFrom(message);
+                    Message.PublishRequest pubReq = wrapper.getPubReq();
+                    if (new String(pubReq.getData().toByteArray(), StandardCharsets.UTF_8).equals("FIN")) {
+                        break;
+                    }
+                    assertEquals(topic, pubReq.getTopic());
+                    assertArrayEquals(data, pubReq.getData().toByteArray());
+                    length = dis.readInt();
+                }
+                dis.close();
+                socket.close();
+                serverSocket.close();
+            } catch (IOException e) {
+                fail(e.getMessage());
+            }
+        });
+        t.start();
+        Producer producer = new Producer("localhost", 1024);
+        for (int i = 0; i < 100000; i++) {
+            producer.send(topic, data);
+        }
+        producer.send(topic, "FIN".getBytes(StandardCharsets.UTF_8));
         producer.close();
         try {
             t.join();
